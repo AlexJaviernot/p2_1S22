35911ab049b8256170d48dfbabf98a506239a912
AlexJaviernot <144191141+AlexJaviernot@users.noreply.github.com>
2023-09-06 18:32:07 -0600

    Add files via upload

---
+++ p2_1S22.java
@@ -0,0 +1,8 @@
+class p2_1S22
+     public static void main(String[] args) {
+		System.out.println("Estamos empezando a programar");
+		System.out.println("Alexander Javier Cruz Madrigal");
+		System.out.println("18 años");
+		System.out.println("alexanderj.cruz@gmail.com");
+		System.out.println("Basquetboll");
+		System.out.println("Negro")
